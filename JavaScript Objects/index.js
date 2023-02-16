6ddc3565abd841a416b9c1add9d51ed6552d1366
abraham <agthankachan007@gmail.com>
2023-02-16 23:57:52 +0530

    js update

--- JavaScript Objects/index.js
+++ JavaScript Objects/index.js
@@ -28,6 +28,7 @@ function addStudents(name,id) {
     return classObj;
 }
 console.log(addStudents("Abraham","28"))
+console.log(addStudents("Tom","30"))
 
 
 
@@ -38,4 +39,44 @@ function addMarks(subject,mark) {
     classObj["students"][0]["marks"].push({"subject":subject,"mark":mark});
 }
 addMarks("English",85)
+// console.log(classObj)
+
+
+// 3. Enter mark for a subject for multiple students
+function addMarksMultiple(subject,markList,studentList) {
+    for (let i = 0; i < classObj["students"].length; i++) {
+     if (studentList.includes(classObj["students"][i]["name"])) {
+            classObj["students"][i]["marks"]=classObj["students"][i]["marks"] || [];
+            classObj["students"][i]["marks"].push({"subject":subject,"mark":markList[studentList.indexOf(classObj["students"][i]["name"])]});
+    }
+    }
+    }
+
+addMarksMultiple("Maths",[81,82],["Abraham","Tom"])
 console.log(classObj)
+
+
+
+// 4. Edit mark for a particular subject of a student
+function editMark(subject,newMark,studentName) {
+        for (let i = 0; i < classObj["students"].length; i++) {
+            if (studentName===classObj["students"][i]["name"]) {
+                for (let j = 0; j < classObj["students"][i]["marks"].length; j++) {
+                    if (subject===classObj["students"][i]["marks"][j]["subject"]) {
+                        classObj["students"][i]["marks"][j]["mark"]=newMark;
+                    }
+                }
+            }
+    }
+}
+editMark("English",97,"Abraham")
+console.log(classObj)
+
+
+
+// 5. Change the class teacher of a class
+function changeTeacher(teacher) {
+    classObj["teacherName"]=teacher;
+    return classObj;
+}
+console.log(changeTeacher("Preethy"));
